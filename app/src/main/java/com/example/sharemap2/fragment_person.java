ae974c5ea4b7b7a193b6ae36995f2eeec8875091
Takeshi Shimizu <takesick213@gmail.com>
2020-01-27 18:15:25 +0900

    first commit from takeshi

--- app/src/main/java/com/example/sharemap2/fragment_person.java
+++ app/src/main/java/com/example/sharemap2/fragment_person.java
@@ -14,7 +14,6 @@ import android.view.ViewGroup;
 
 public class fragment_person extends Fragment {
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
